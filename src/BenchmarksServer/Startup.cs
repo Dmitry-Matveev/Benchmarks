240c6c22de80bee306aa3021839c886af8de9b79
Sebastien Ros <sebastienros@gmail.com>
2018-06-05 10:35:19 -0700

    Testing sigint in middleware

--- src/BenchmarksServer/Startup.cs
+++ src/BenchmarksServer/Startup.cs
@@ -174,6 +174,93 @@ namespace BenchmarkServer
             {
                 return context.Response.WriteAsync("OK!");
             });
+
+            app.Map("/sigint", (b) =>
+            {
+                b.Run(async context =>
+                {
+                    var process = new Process()
+                    {
+                        StartInfo = {
+                        FileName = "perfcollect",
+                        Arguments = $"collect benchmarks",
+                        WorkingDirectory = ".",
+                        RedirectStandardOutput = true,
+                        UseShellExecute = false,
+                    }
+                    };
+
+                    process.OutputDataReceived += (_, e) =>
+                    {
+                        if (e != null && e.Data != null)
+                        {
+                            Console.WriteLine($"[{process.Id}] {e.Data}");
+                        }
+                    };
+
+                    process.Start();
+                    process.BeginOutputReadLine();
+
+                    await Task.Delay(5000);
+
+                    if (!process.HasExited)
+                    {
+                        var processId = process.Id;
+
+                        Console.WriteLine($"Stopping script");
+
+                        Mono.Unix.Native.Syscall.kill(process.Id, Mono.Unix.Native.Signum.SIGINT);
+
+                        var delay = Task.Delay(5000);
+
+                        while (!process.HasExited && !delay.IsCompletedSuccessfully)
+                        {
+                            await Task.Delay(1000);
+                        }
+
+                        if (!process.HasExited)
+                        {
+                            Console.WriteLine($"Forcing process to stop ...");
+                            process.CloseMainWindow();
+
+                            if (!process.HasExited)
+                            {
+                                process.Kill();
+                            }
+
+                            process.Dispose();
+
+                            do
+                            {
+                                Console.WriteLine($"Waiting for process {processId} to stop ...");
+
+                                await Task.Delay(1000);
+
+                                try
+                                {
+                                    process = Process.GetProcessById(processId);
+                                    process.Refresh();
+                                }
+                                catch
+                                {
+                                    process = null;
+                                }
+
+                            } while (process != null && !process.HasExited);
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Process has stopped by itself");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Process is not running");
+                    }
+
+                }
+                );
+            });
         }
 
         public static int Main(string[] args)
