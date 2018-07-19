6a93bbe9f370e725cd6914c8fde7b386fd5e71e7
Justin Kotalik <jukotali@microsoft.com>
2018-07-18 17:59:35 -0700

    test

--- src/Benchmarks/Middleware/PlaintextMiddleware.cs
+++ src/Benchmarks/Middleware/PlaintextMiddleware.cs
@@ -38,9 +38,6 @@ namespace Benchmarks.Middleware
             response.StatusCode = 200;
             response.ContentType = "text/plain";
             response.ContentLength = payloadLength;
-            for (var i = 0; i < 10; i++) {
-                Console.WriteLine("This is a test log to verify how bad perf changes between runs");
-            }
             return response.Body.WriteAsync(_helloWorldPayload, 0, payloadLength);
         }
     }
